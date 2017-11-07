35383b1b4bb94e178978a51be4f528f428172497
Amy McGovern <dramymcgovern@gmail.com>
2017-11-07 16:44:12 -0600

    fixing color print

--- utils/colorPrint.py
+++ utils/colorPrint.py
@@ -9,20 +9,20 @@ def color_print(print_str, type="NONE"):
 
     if (type is "ERROR"):
         # red
-        print('\033[38;5;196m' + print_str + '\033[0m')
+        print('\033[38;5;196m %s \033[0m' % print_str)
 
     elif (type is "WARN"):
         # orange
-        print('\033[38;5;202m' + print_str + '\033[0m')
+        print('\033[38;5;202m %s \033[0m' % print_str)
 
     elif (type is "SUCCESS"):
         # green
-        print('\033[38;5;22m' + print_str + '\033[0m')
+        print('\033[38;5;22m %s \033[0m' % print_str)
 
     elif (type is "INFO"):
         # blue
-        print('\033[38;5;33m' + print_str + '\033[0m')
+        print('\033[38;5;33m %s \033[0m' % print_str)
 
     elif (type is "NONE" or type is "DEFAULT"):
         # black
-        print('\033[38;5;33m' + print_str + '\033[0m')
+        print('\033[38;5;33m %s \033[0m' % print_str)
