17b88fda030af7dd820be68809c5910d1f545dab
Victor804 <victor.vaucel@gmail.com>
2018-07-28 22:55:59 +0200

    updated exemples/demoSwingDirectFlight

--- examples/demoSwingDirectFlight.py
+++ examples/demoSwingDirectFlight.py
@@ -6,10 +6,10 @@ Author: Victor804
 
 from pyparrot.Minidrone import Swing
 
-# you will need to change this to the address of YOUR mambo
+# you will need to change this to the address of YOUR swing
 swingAddr = "e0:14:04:a7:3d:cb"
 
-# make my mambo object
+# make my swing object
 swing = Swing(swingAddr)
 
 print("trying to connect")
@@ -26,29 +26,14 @@ if (success):
     print("taking off!")
     swing.safe_takeoff(5)
 
-    print("Flying direct: going forward (positive pitch)")
-    swing.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=1)
+    print("plane forward")
+    swing.set_plane_gear_box("gear_1")
+    swing.set_flying_mode("plane_forward")
 
-    print("Showing turning (in place) using turn_degrees")
-    swing.turn_degrees(90)
-    swing.smart_sleep(2)
-    swing.turn_degrees(-90)
-    swing.smart_sleep(2)
+    swing.smart_sleep(1)
 
-    print("Flying direct: yaw")
-    swing.fly_direct(roll=0, pitch=0, yaw=50, vertical_movement=0, duration=1)
-
-    print("Flying direct: going backwards (negative pitch)")
-    swing.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=0.5)
-
-    print("Flying direct: roll")
-    swing.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0, duration=1)
-
-    print("Flying direct: going up")
-    swing.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=50, duration=1)
-
-    print("Flying direct: going around in a circle (yes you can mix roll, pitch, yaw in one command!)")
-    swing.fly_direct(roll=25, pitch=0, yaw=50, vertical_movement=0, duration=3)
+    print("quadricopter")
+    swing.set_flying_mode("quadricopter")
 
     print("landing")
     swing.safe_land(5)
